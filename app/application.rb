2a83c2f5b4b23ce85a29d34f6db70d79502462ba
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-24 11:50:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,13 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      search_term = req.params["q"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
