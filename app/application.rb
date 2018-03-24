2caa71b1489a1b6521f355ecdd1c09cad24b9529
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-24 12:15:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
+      binding.pry
       item = req.params["q"]
       if @@items.include?(item)
         @@cart << item
